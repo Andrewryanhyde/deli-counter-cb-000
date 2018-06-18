7a1b04df48704c6b6412630ab20007783a988486
Andrew Hyde <andrewryanhyde@hotmail.co.uk>
2018-06-18 11:19:03 +0000

    automatically backed up by learn

--- deli_counter.rb
+++ deli_counter.rb
@@ -1,15 +1,18 @@
 # Write your code here.
 katz_deli = []
-def line(queue)
+
+def line(queue, position, name)
   if queue.length === 0
     puts "The line is currently empty."
   else
-    puts queue
+    queue.each do |customer|
+    puts "The line is currently: #{position}. #{name}"
+    end
   end
 end
 
 def take_a_number(queue, name)
   queue.push(name)
-  index = queue.find_index(name)
-  puts "#{name}, your position is #{index}"
+  position = queue.find_index(name)
+  puts "#{name}, your position is #{position}"
 end
